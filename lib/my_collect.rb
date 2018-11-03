2e718e36bffa6732346ef74b3cfa4905da91000a
Greg Morgan <gregm9999@gmail.com>
2018-11-03 21:12:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+student = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ["ruby", "javascript", "python", "objective-c"]
 
+def my_collect(collection)
+  newAr=[]
+    i=0
+    while i<collection.length
+      newAr << yield(collection[i])
+      i+=1
+    end
+  newAr
+end
 
+my_collect(collection)do |lang|
+   lang.upcase
+end
+
+my_collect(student) do |student|
+  student.split(" ")[0]
+end
