bf1f3c51566fb44dccd54ffd22cb857fe92f47c7
Greg Morgan <gregm9999@gmail.com>
2018-11-03 21:05:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,12 +3,14 @@ collection = ["ruby", "javascript", "python", "objective-c"]
 
 def my_collect(collection)
   if block_given?
-  i=0
-  while i<collection.length
-    yield(collection[i])
-    i+=1
+    newAr=[]
+    i=0
+    while i<collection.length
+      yield(collection[i])
+      i+=1
+    end
+    collection
   end
-  collection
 end
 
 my_collect(collection)
